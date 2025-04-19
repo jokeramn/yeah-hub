7b6177e26976b5918db820add7f8fcac4ee517d9
Sergei Prokopchuk <seraapa@mail.ru>
2025-04-19 18:46:44 +0300

    first webpack settings

---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: 'development',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html', // можешь создать шаблон в public
+        }),
+    ],
+};
